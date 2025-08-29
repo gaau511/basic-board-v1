b8fd12b615e1c6aed7b1e697b786e0b8b56ebb35
gaau <gaau511@gmail.com>
2025-08-29 09:35:40 +0900

    feat(*): 프로젝트 초기 세팅 (#1)

---
+++ src/main/java/com/imonboard/basic_board_v1/BasicBoardV1Application.java
@@ -0,0 +1,13 @@
+package com.imonboard.basic_board_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBoardV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBoardV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/imonboard/basic_board_v1/BasicBoardV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imonboard.basic_board_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
